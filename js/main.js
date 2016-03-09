b18b82fc3b1340ebd52e57b958a557f0a01fe5de
SarahTierney <tierney.s@live.com>
2016-03-08 19:31:36 -0500

    Added "berries" thing

--- js/main.js
+++ js/main.js
@@ -36,6 +36,50 @@ var redCurrant = {
 
 // Write your function here, name it `writeBerries`
 
+var writeBerries = function (berries) {
+  document.write('<h1>Berries</h1>');
+
+  bugs.forEach(function (berry) {
+    document.write('<h2>' + berry.name + '</h2>');
+    document.write('<dl>');
+    document.write('<dt>Grows on a shrub?</dt>');
+    if (berry.onShrub) {
+      document.write('<dd>Yes.</dd>');
+    } else {
+      document.write('<dd>No.</dd>');
+    }
+
+    document.write('<dd><i>' + berry.onShrub + '</i></dd>');
+    document.write('<dd>' + berry.energy[0] + ' kj</dd>');
+    document.write('<dd><i>' + berry.carbs[0] + ' g</i></dd>');
+    document.write('<dd><i>' + berry.protein[0] + ' g</i></dd>');
+    document.write('<dt>Size</dt>');
+    document.write('<dt>Eats</dt>');
+
+    document.write('<dd>');
+
+
+
+    if (bug.food.eatsDung) {
+      document.write('Dung');
+    }
+
+    if (bug.food.eatsPlants) {
+      document.write('Plants');
+    }
+
+    document.write('</dd>');
+
+    document.write('</dl>');
+    document.write('</li>');
+  });
+
+  document.write('</ol>');
+};
+
+writeInsects(insects);
+
+
 
 // ##########################################################
 // START DO NOT CHANGE ZONE
